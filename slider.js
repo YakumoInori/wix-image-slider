d01503d51bb6d46673b3244d5c52049fb602e057
YakumoInori <141685172+YakumoInori@users.noreply.github.com>
2025-05-09 15:15:12 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,17 +1,8 @@
 const images = document.querySelectorAll("#slider img");
-const slider = document.getElementById("slider");
 let index = 0;
 
-function updateSlider() {
-  images.forEach(img => img.classList.remove('active'));
-
-  images[index].classList.add('active');
-
-  const type = images[index].dataset.type;
-  slider.className = `slider ${type}`;
-  
+setInterval(() => {
+  images[index].classList.remove("active");
   index = (index + 1) % images.length;
-}
-
-setInterval(updateSlider, 5000); // 每5秒自动轮播一次
-window.onload = updateSlider; // 确保加载后立即执行一次
+  images[index].classList.add("active");
+}, 5000);

5a956335e78a49553d7887a0c71a0e79f06eb8a9
YakumoInori <141685172+YakumoInori@users.noreply.github.com>
2025-05-09 13:37:57 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -7,7 +7,7 @@ setInterval(() => {
   index = (index + 1) % images.length;
   images[index].classList.add("active");
 
-  // 动态切换模板样式
+  // 自动切换容器样式
   const type = images[index].dataset.type;
   slider.className = type;
 }, 5000);
